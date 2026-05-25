63888d13e8045b59b71f3035e63a37e644bc259c
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-05-25 16:48:49 +0600

    learning

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,11 +1,37 @@
-import express, { type Application, type Request, type Response } from 'express'
-const app : Application = express()
-const port = 5000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+const app: Application = express();
+const port = 5000;
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
+app.use(express.json()); // for parsing application/json so that we can access req.body in our routes
+app.use(express.text());
+app.use(express.urlencoded({ extended: true })); // extended: true allows to send nested objects in the request body
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_doZCwaVn1tm4@ep-calm-sound-apd1dng0-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+app.get("/", (req: Request, res: Response) => {
+  // res.send('Hello World!')
+  res.status(200).json({
+    message: "Hello World!",
+    status: 200,
+  });
+});
+app.post("/", async (req: Request, res: Response) => {
+  // console.log(req.body)
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created successfully!",
+    status: 201,
+    data: { name, email, password },
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
