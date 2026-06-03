2ba6e6743d219d9a1bb55dc541b6c3aea802327e
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-06-03 21:21:43 +0600

    move to user modules

--- express/src/app.ts
+++ express/src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 
 import { pool } from "./db";
+import { userRouter } from "./modules/user/user.route";
 const app: Application = express();
 
 app.use(express.json()); // for parsing application/json so that we can access req.body in our routes
@@ -18,26 +19,9 @@ app.get("/", (req: Request, res: Response) => {
     status: 200,
   });
 });
-app.post("/api/users", async (req: Request, res: Response) => {
-  // console.log(req.body)
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(
-      `INSERT INTO users (name, email, password,age) VALUES ($1, $2, $3,$4) RETURNING *`,
-      [name, email, password, age],
-    );
-    console.log(name, email, password, age);
-    res.status(201).json({
-      message: "User created successfully!",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: error.message,
-      error: error,
-    });
-  }
-});
+
+app.use('/api/users', userRouter);
+
 
 app.get("/api/users", async (req: Request, res: Response) => {
   try {
--- express/src/modules/user/user.route.ts
+++ express/src/modules/user/user.route.ts
@@ -1,5 +1,27 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+import { pool } from "../../db";
 
 const router = Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  // console.log(req.body)
+  const { name, email, password, age } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (name, email, password,age) VALUES ($1, $2, $3,$4) RETURNING *`,
+      [name, email, password, age],
+    );
+    console.log(name, email, password, age);
+    res.status(201).json({
+      message: "User created successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 export const userRouter = router;
