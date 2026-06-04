88d48bf6df8d903f906a105ad973644bd22d4d97
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-06-04 15:09:29 +0600

    refactoring user modules

--- express/src/modules/user/user.route.ts
+++ express/src/modules/user/user.route.ts
@@ -1,27 +1,11 @@
-import { Router, type Request, type Response } from "express";
-import { pool } from "../../db";
+import { Router } from "express";
+import { userController } from "./user.controller";
+
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  // console.log(req.body)
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(
-      `INSERT INTO users (name, email, password,age) VALUES ($1, $2, $3,$4) RETURNING *`,
-      [name, email, password, age],
-    );
-    console.log(name, email, password, age);
-    res.status(201).json({
-      message: "User created successfully!",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: error.message,
-      error: error,
-    });
-  }
-});
+router.post("/", userController.createUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getSingleUser);
 
 export const userRouter = router;
