b2e0ee5f492c0f800d51cbff4b5b07eb59f7d9de
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-06-18 22:46:20 +0600

    Enhance authentication middleware with user status checks and add TypeScript definitions for request user property

--- express/src/middleware/auth.ts
+++ express/src/middleware/auth.ts
@@ -5,24 +5,44 @@ import { pool } from "../db";
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
     // console.log("Authenticating user...");
-    const token = req.headers.authorization;
-    if (!token) {
-      res.status(401).json({
-        success: false,
-        message: "Unauthorized: No token provided",
-      });
-    }
-    const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload;
-    // console.log("Decoded token:", decoded);
-    const userData = await pool.query(
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized: No token provided",
+        });
+      }
+      const decoded = jwt.verify(
+        token as string,
+        config.secret as string,
+      ) as JwtPayload;
+      // console.log("Decoded token:", decoded);
+      const userData = await pool.query(
         `SELECT * FROM users WHERE email = $1`,
-        [decoded.email]
-    );
-    // console.log("User data:", userData);
-    // console.log("User data:", userData.rows[0]);
-    const user = userData.rows[0];
-    console.log("User data:", user);
-    next();
+        [decoded.email],
+      );
+      // console.log("User data:", userData);
+      // console.log("User data:", userData.rows[0]);
+      const user = userData.rows[0];
+      if (userData.rowCount === 0) {
+        res.status(404).json({
+          success: false,
+          message: "Unauthorized access: User not found",
+        });
+      }
+      if (user.is_active === false) {
+        res.status(403).json({
+          success: false,
+          message: "Forbidden: User account is inactive",
+        });
+      }
+      console.log("User data:", user);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      next(error);
+    }
   };
 };
 
